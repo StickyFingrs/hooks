9730bff317c7454e5af543151f115825c9ee3f23
Gordon Liew <gordonliewgl@gmail.com>
2021-04-22 17:00:23 +0800

    Added testing list of items

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+	{
+		title: "What is React?",
+		content: "Blachfiawjeoifasdnuifn iwua fasduifiuhweiuf dkfloawefoi awjosdfmio awejf oawifjoasijdf"
+	}, 
+	{
+		title: "Why is React?",
+		content: "Blaasdfasdfwe wefd htrh gfncbv sdfg erg dvbaergae rfijdf"
+	}, 
+	{
+		title: "Who is React?",
+		content: "nstr aiosdjfio awjfo iuahuidu fiawn ifuaweuifh asdiofniuawehfi asdfol aweo basd"
+	}
+]
 
 class App extends React.Component {
 	render () {
-		return <h1>Hello</h1>
+		return (
+			<div>
+				<Accordion items={items} />
+			</div>
+		);
 	};
 }
 
