1b26723ba78a010b33826cb8bf2571c3a2e681e3
Gordon Liew <gordonliewgl@gmail.com>
2021-04-27 21:25:11 +0800

    Added opening and closing dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,15 @@
 import React, {useEffect, useState, useRef} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+	// Determines if dropdown is open or not
+	const [open, setOpen] = useState(false);
+
 	const renderedOptions = options.map((option) => {
+		// Don't show the option in the dropdown if it is currently selected
+		if (option.value === selected.value) {
+			return null;
+		}
+
 		return (
 			<div
 				key={option.value}
@@ -17,10 +25,13 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
 		<div className="ui form">
 			<div className="field">
 				<label className="label">Select a Colour</label>
-				<div className="ui selection dropdown visible active">
+				<div 
+					onClick={() => setOpen(!open)}
+					className={`ui selection dropdown ${open == true ? "visible active" : ""}`}
+				>
 					<i className="dropdown icon"></i>
 					<div className="text">{selected.label}</div>
-					<div className="menu visible transition">
+					<div className={`menu ${open == true ? "visible transition" : ""}`}>
 						{renderedOptions}
 					</div>
 				</div>
